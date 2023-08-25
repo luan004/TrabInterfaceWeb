4040bf7c132af848eebc0bffc3febcd7fed05992
Luan Gabriel <luan.gabriel.ifpr@gmail.com>
2023-08-24 23:09:05 -0300

    24-08

--- src/js/game.js
+++ src/js/game.js
@@ -24,16 +24,22 @@ function check(tried) {
     return tent;
 }
 
+var dica = 0;
 $('#dica').click(function () {
-    const sorted = Math.floor(Math.random() * word.length);
+    if (dica < 3) {
+        const sorted = Math.floor(Math.random() * word.length);
     
-    let tent = '';
-    for (let i = 0; i < word.length; i++) {
-        if (i == sorted) {
-            tent += wordL[i] + ' ';
-        } else {
-            tent += '_ ';
+        let tent = '';
+        for (let i = 0; i < word.length; i++) {
+            if (i == sorted) {
+                tent += wordL[i] + ' ';
+            } else {
+                tent += '_ ';
+            }
         }
+        $('#card').append(`<h3>${tent}</h3>`);
+        dica++;
+        $('#numdica').html(`${dica}`);
     }
 });
 
@@ -43,11 +49,15 @@ $('form').submit(function (e) {
     const tried = $('form input').val();
     if (tried.length == word.length) {
         const tent = check(tried);
-        $('#card').append(`<h3>${tent}</h3>`);
 
+        if (tent != wordUnderlined && tent.replace(/ /g, '') != word) {
+            $('#card').append(`<h3>${tent}</h3>`);
+        }
 
         if (tent.replace(/ /g, '') == word) {
+            $('#card').append(`<h2><i class="fa fa-star" style="margin-right:15px"></i>${tent.trim()}<i class="fa fa-star" style="margin-left:15px"></h2>`);
             $('#card').append(`<h3>Parabéns, você acertou!</h3>`);
         }
+        $('form input').val('');
     }
 });
